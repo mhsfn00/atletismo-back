2ea70278239e31dfc83dc8838cf3215634f322e7
unknown <mhsfn00@gmail.com>
2023-12-28 11:20:46 -0300

    small changes

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ async function start () { // Function so we dont need to rewrite the database co
     app.use(express.json()); // Telling express to parse the requests using json (i think)
     
     const dbPassword = process.env.MONGO_PASSWORD; // Getting password from .env file using dotenv (gitignore)
-    const url = `mongodb+srv://admin:${dbPassword}@cluster0.bueqvia.mongodb.net/?retryWrites=true&w=majority`;
+    const dbKey = process.env.MONGO_KEY;
+    const url = `mongodb+srv://admin:${dbPassword}@cluster0.${dbKey}.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(url);
 
     await client.connect(); // Connecting to mongodb
@@ -15,7 +16,7 @@ async function start () { // Function so we dont need to rewrite the database co
     
     app.get('/api/posts', async (req, res) => {
         const posts = await db.collection('posts').find({}).toArray(); // Find all in collection "posts" (looks like an array of objects)
-        res.send(posts);
+        res.json(posts);
     })
     
     app.get('/api/post/:postId', async (req, res) => {
