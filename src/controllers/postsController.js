465de935930c886c186c840a33e24cf588ad6f9f
unknown <mhsfn00@gmail.com>
2024-10-24 21:34:56 -0300

    posts CRUD finished, pending tests and refactoring

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -16,17 +16,21 @@ const createPost = async (req, res) => {
     }
 
     const newPostJSon = req.body;
+    console.log("one");
 
     try {
         if (newPostJSon.mainPost) { //if flagged as main post (will be default on the frontend)
-            if (MainPost.find().length() == 0) {
-                const dbRes = await MainPost.create(newPostJSon);
+            let dbRes = await MainPost.find();
+            if (dbRes.length == 0) {
+                dbRes = await MainPost.create(newPostJSon);
                 return res.status(201).json(dbRes);
             } else {
-                const previousMainPost = await MainPost.find()[0]; //get current main post
-                const dbResCreate = MainPost.create(previousMainPost); //save current main post as normal post
+                dbRes = await MainPost.find(); //get current main post
+                const previousMainPost = dbRes[0];
+                console.log(previousMainPost);
+                await MainPost.create(previousMainPost); //save current main post as normal post
                 const dbResUpdate = await MainPost.findOneAndUpdate({}, newPostJSon); //update main post with new post
-                return res.status(201).json(dbResCreate, dbResUpdate);
+                return res.status(201).json(dbResUpdate);
             }
         } else { //not flagged as main, keeps the previous main post
             const dbRes = await Post.create(newPostJSon);
@@ -63,12 +67,66 @@ const updatePost = async (req, res) => {
     }
 }
 
-const deletePost = (req, res) => {
-    
+const deletePost = async (req, res) => {
+    if (!req?.body) {
+        return res.status(400).json({ 'message': 'Bad request' });
+    } else if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'Empty request body'});
+    }
+
+    const postId = req.body._id;
+    if (!postId) {
+        return res.status(400).json({ 'message': 'Could not find post id'});
+    }
+
+    try {
+        let dbRes = await MainPost.find({ _id: `${postId}`});
+        if (dbRes.length != 0) {
+            await MainPost.deleteOne({ _id: `${postId}`});
+            return res.status(200).json({ 'message': 'Post deleted successfuly'});
+        } else {
+            dbRes = await Post.find({ _id: `${postId}`});
+            if (dbRes.length != 0) {
+                await Post.deleteOne({ _id: `${postId}`});
+                return res.status(200).json({ 'message': 'Post deleted successfuly'});
+            } else {
+                return res.status(400).json("Post was not found");
+            }
+        }
+    } catch (err) {
+        console.log(err.message);
+        return res.status(400).json(err.message);
+    }
 }
 
-const getPostById = (req, res) => {
+const getPostById = async (req, res) => {
+    if (!req?.body) {
+        return res.status(400).json({ 'message': 'Bad request' });
+    } else if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'Empty request body'});
+    }
 
+    const postId = req.body._id;
+    if (!postId) {
+        return res.status(400).json({ 'message': 'Could not find post id'});
+    }
+
+    try {
+        let dbRes = await MainPost.find({ _id: `${postId}`});
+        if (dbRes.length != 0) {
+            return res.status(200).json(dbRes[0]);
+        } else {
+            dbRes = await Post.find({ _id: `${postId}`});
+            if (dbRes.length != 0) {
+                return res.status(200).json(dbRes[0]);
+            } else {
+                return res.status(400).json("Post was not found");
+            }
+        }
+    } catch (err) {
+        console.log(err.message);
+        return res.status(400).json(err.message);
+    }
 }
 
 module.exports = {
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -6,50 +6,9 @@ router.route('/')
     .get(postsController.getAllPosts)
     .post(postsController.createPost)
     .put(postsController.updatePost)
-    .delete()
+    .delete(postsController.deletePost)
 
-router.route('/:id')
-    .get()
-
-router.post('/newPost', async (req, res) => {
-    try {
-        // add as main post if needed
-        const dbRes = await MainPost.find();
-        if (dbRes.length == 0) {
-            console.log("exists but its empty");
-        }
-        console.log("responsE--> ", dbRes);
-        // else add as normal post 
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-router.get('/:postId', async (req, res) => {
-    const db = req.app.get('db');
-    const postId = req.params.postId; // postId from request
-    const post = await db.collection('mainPost').findOne({ id: postId }) ? // Checking for the post id in the mainPost collection first
-        await db.collection('mainPost').findOne({ id: postId }) : await db.collection('posts').findOne({ id: postId });  
-
-    res.json(post);
-});
-
-router.put('/:postId/editPost', async (req, res) => {
-    const db = req.app.get('db');
-    const modifiedPost = req.body;
-    const mainPost = await db.collection('mainPost').findOne({ id: modifiedPost.id }) ? true : false;
-    const collection = mainPost ? 'mainPost' : 'posts';
-
-    const mongoResponse = await db.collection(`${collection}`).updateOne({id: modifiedPost.id },
-        {$set: {
-            title: modifiedPost.title,
-            imageLink: modifiedPost.imageLink,
-            text: modifiedPost.text,
-            lastModified: new Date().toDateString(),
-        }}
-    );
-    
-    res.send(JSON.stringify(mongoResponse));
-});
+router.route('/getById')
+    .get(postsController.getPostById)
 
 module.exports = router;
