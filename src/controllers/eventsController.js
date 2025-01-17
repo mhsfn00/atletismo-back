ed209072af4dbed20426c4198c145dfe9262a644
mhsfn00 <mhsfn00@gmail.com>
2025-01-17 15:27:05 -0300

    Finished events CRUD, pending testing and refactoring

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -21,7 +21,23 @@ const getEvents = async (req, res) => {
 }
 
 const getById = async (req, res) => {
-    return res.status(200).json({"message" : "getting specific event with id"});
+    if (!req?.body) {
+        return res.status(400).json({ 'message': 'Bad request' });
+    } else if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'Empty request body' });
+    }
+
+    const eventId = req.body._id;
+    if(!eventId) {
+        return res.status(400).json({'message' : 'event id needs to be provided'});
+    }
+
+    try {
+        const dbRes = await Event.find({ _id: `${eventId}`});
+        return res.status(200).json(dbRes);
+    } catch (err) {
+        return res.status(400).json(err.message);
+    }
 }
 
 const createEvent = async (req, res) => {
@@ -42,11 +58,41 @@ const createEvent = async (req, res) => {
 }
 
 const updateEvent = async (req, res) => {
-    return res.status(200).json({"message" : "updating event with id"});
+    if (!req?.body) {
+        return res.status(400).json({ 'message': 'Bad request' });
+    } else if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'Empty request body' });
+    }
+
+    const updatedEvent = req.body;
+    console.log(updatedEvent)
+
+    try {
+        const dbRes = await Event.findOneAndUpdate({ _id: `${updatedEvent._id}`}, updatedEvent);
+        res.status(200).json(dbRes);
+    } catch (err) {
+        res.status(400).json(err.message);
+    }
 }
 
 const deleteEvent = async (req, res) => {
-    return res.status(200).json({"message" : "deleting event with id"});
+    if (!req?.body) {
+        return res.status(400).json({ 'message': 'Bad request' });
+    } else if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'Empty request body' });
+    }
+
+    const eventId = req.body._id;
+    if(!eventId) {
+        return res.status(400).json({'message' : 'event id needs to be provided'});
+    }
+
+    try {
+        const dbRes = await Event.findOneAndDelete({ _id: `${eventId}`});
+        return res.status(200).json({ 'message' : `deleted event with id ${dbRes._id}`});
+    } catch (err) {
+        return res.status(400).json(err.message);
+    }
 }
 
 module.exports = {
